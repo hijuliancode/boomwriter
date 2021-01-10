23a42a5d4ee08694b3e210edbae89f6d48000cf9
hijuliancode <hijuliansosa@protonmail.com>
2021-01-09 23:48:34 -0500

    BoomwriterApp: Section Home Testimonials: Mobile: styles

--- src/components/Home/SliderTestimonials/index.jsx
+++ src/components/Home/SliderTestimonials/index.jsx
@@ -3,6 +3,12 @@ import HOME_API from "../../../services/homeAPI.service";
 
 import {
   SliderTestimonialsElm,
+  Title,
+  Description,
+  Testimonials,
+  TestimonialCard,
+  TwitterIcon,
+  User,
 } from './style';
 
 const SliderTestimonialsComponent = () => {
@@ -10,7 +16,30 @@ const SliderTestimonialsComponent = () => {
 
   return (
     <SliderTestimonialsElm>
-      SliderTestimonialsComponent works!
+      <Title>{ sliderTestimonialsData.title }</Title>
+      <Description>{ sliderTestimonialsData.description }</Description>
+      
+      {
+        sliderTestimonialsData.testimonials && (
+          <Testimonials>
+            {
+              sliderTestimonialsData.testimonials.map(testm => (
+                <TestimonialCard>
+                  <TwitterIcon href={testm.tweetUrl} target="_blank">
+                    <img src="https://boomwriter.com/static/media/twitter.591ff2a4.svg" alt="" />
+                  </TwitterIcon>
+                  <p>{ testm.text }</p>
+                  <User href={ testm.tweetUrl } title={ testm.user.username + ' - ' +  testm.user.occupation} target="_blank" >
+                    <img src={ testm.user.avatar } alt={ testm.user.username } />
+                    <strong>{ testm.user.username }</strong>
+                    <p>{ testm.user.occupation }</p>
+                  </User>
+                </TestimonialCard>
+              ))
+            }
+          </Testimonials>
+        )
+      }
     </SliderTestimonialsElm>
   );
 }
--- src/components/Home/SliderTestimonials/style.js
+++ src/components/Home/SliderTestimonials/style.js
@@ -3,7 +3,86 @@ import styled from 'styled-components';
 const SliderTestimonialsElm = styled.div`
   @media ${props => props.theme.MediaQueries.lg} {}
 `;
+const Title = styled.h2`
+  color: ${props => props.theme.colors.dark};
+  font-size: 40px;
+  line-height: 40px;
+  text-align: center;
+`;
+const Description = styled.p`
+  color: ${props => props.theme.colors.brand1}
+  font-size: 18px;
+  line-height: 26px;
+  margin-bottom: 24px;
+  text-align: center;
+  @media ${props => props.theme.MediaQueries.lg} {
+    text-align: left;
+  }
+`;
+const Testimonials = styled.div``;
+const TestimonialCard = styled.article`
+  background-color: ${props => props.theme.colors.white};
+  border-radius: 24px;
+  box-shadow: 0 0 30px -5px rgba(5,95,230,.5);
+  color: ${props => props.theme.colors.brand1};
+  margin: 0 auto 16px;
+  padding: 1.75em;
+  : > p {
+    font-size: 16px;
+    line-height: 20px;
+    margin: 0;
+  }
+`;
+const TwitterIcon = styled.a`
+  align-items: center;
+  background-color: ${props => props.theme.colors.primary};
+  border-radius: 20px;
+  color: ${props => props.theme.colors.white};
+  display: flex;
+  margin-bottom: 16px;
+  height: 60px;
+  justify-content: center;
+  position: relative;
+  width: 60px;
+  img {
+    max-width: 100%;
+    width: 28px;
+  }
+`;
+const User = styled.a`
+  display: grid;
+  column-gap: 16px;
+  grid-template-areas:
+    "area_image area_name"
+    "area_image area_occupation";
+  grid-template-columns: 80px 1fr;
+  img {
+    border-radius: 100%;
+    display: block;
+    grid-area: area_image;
+    max-width: 100%;
+    object-fit: cover;
+    overflow: hidden;
+    width: 80px;
+  }
+  strong {
+    align-self: flex-end;
+    color: ${props => props.theme.colors.brand1};
+    font-size: 20px;
+    grid-area: area_name;
+  }
+  p {
+    color: ${props => props.theme.colors.muted};
+    grid-area: area_occupation;
+  }
+`;
 
 export {
   SliderTestimonialsElm,
+  Title,
+  Description,
+  Testimonials,
+  TestimonialCard,
+  TwitterIcon,
+  User,
 }
