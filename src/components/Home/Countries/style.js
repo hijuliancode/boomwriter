80cb10a478517d92fbf5baf0d29ba8e5eecc8ccc
hijuliancode <hijuliansosa@protonmail.com>
2021-01-10 16:38:24 -0500

    BoomwriterApp: Countries desktop

--- src/components/Home/Countries/style.js
+++ src/components/Home/Countries/style.js
@@ -3,7 +3,9 @@ import styled from 'styled-components';
 const CountriesElm = styled.div`
   margin-bottom: ${props => props.theme.baseSize * 12}px;
   text-align: center;
-  @media ${props => props.theme.MediaQueries.lg} {}
+  @media ${props => props.theme.MediaQueries.lg} {
+    margin-bottom: ${props => props.theme.baseSize * 24}px;
+  }
 `;
 const Title = styled.h2`
   color: ${props => props.theme.colors.dark};
@@ -12,9 +14,10 @@ const Title = styled.h2`
   margin-bottom: ${props => props.theme.baseSize * 6}px;
   text-align: center;
   @media ${props => props.theme.MediaQueries.lg} {
+    color: ${props => props.theme.colors.brand1};
+    margin-bottom: ${props => props.theme.baseSize * 15}px;
     font-size: 50px;
     line-height: 50px;
-    text-align: left;
   }
 `;
 const CountriesImage = styled.figure`
